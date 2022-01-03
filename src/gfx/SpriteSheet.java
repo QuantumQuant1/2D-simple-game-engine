79d06be1db58b6c23147c57f832de2ea265de399
Kai <kai.borro@gmail.com>
2022-01-03 16:07:08 +0100

    first commit

---
+++ src/gfx/SpriteSheet.java
@@ -0,0 +1,40 @@
+package gfx;
+
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class SpriteSheet {
+	
+	public String path;
+	public int width;
+	public int height;
+
+	public int[] pixels;
+
+	public SpriteSheet(String path) {
+		BufferedImage image = null;
+
+		try {
+			image = ImageIO.read(new FileInputStream(path));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		if (image == null) {
+			return;
+		}
+
+		this.path = path;
+		this.width = image.getWidth();
+		this.height = image.getHeight();
+
+		pixels = image.getRGB(0, 0, width, height, null, 0, width);
+
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = (pixels[i] & 0xff) / 64;
+		}
+	}
+}
